eef2e69544c3e2494a4ab0353ba2e8a2b3b1ef0e
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-07 22:10:35 +0530

    34-string.py

---
+++ 01-assignment-variable/34-string.py
@@ -0,0 +1,15 @@
+Name = "Dev" 
+Age = 25 
+City = "Jaipur" 
+Hobby = "Cooking"
+print("user details :")
+print(f"Name : {Name}")
+print(f"Age : {Age}")
+print(f"City : {City}")
+print(f"Hobby : {Hobby}")
+
+print("""Meet {Name},a {Age}-year-old enthusiast from {City}.
+When not busy with daily tasks,{Name} loves spending time {Hobby}.
+Life in {City} keeps {Name} energetic and curious every single day.
+With coding as a passion, the future looks creative and inspiring for 
+{Name} in the {City} City.""")
