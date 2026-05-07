a77ffb709976548f86c8ef5941d6aaf8d5675910
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-07 22:00:13 +0530

    30-mathematical.py

---
+++ 01-assignment-variable/30-mathematical.py
@@ -0,0 +1,7 @@
+x = 10
+y = 6
+print (f"Expression: (x - y)²")
+print (f"Value of x: {x}")
+print (f"Value of y: {y}")
+simplified_expression = (x - y)**2
+print (f"Simplified expression: {simplified_expression}")
