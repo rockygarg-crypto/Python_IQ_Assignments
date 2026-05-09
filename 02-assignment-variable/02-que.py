8a3b2feafb081fd2cbdd05b8c7db3ac9e9b005c4
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-09 22:34:05 +0530

    02-que.py

---
+++ 02-assignment-variable/02-que.py
@@ -0,0 +1,13 @@
+player1 = int(input("enter run scored by player 1 : "))
+player2 = int(input("enter run scored by player 2 : "))
+player3 = int(input("enter run scored by player 3 : "))
+player4 = int(input("enter run scored by player 4 : "))
+player5 = int(input("enter run scored by player 5 : "))
+
+total_runs = player1 + player2 + player3 + player4 + player5
+average_runs = total_runs / 5
+
+print("total_runs scored:" , total_runs)
+print("average_runs scored:" , average_runs)
+
+
