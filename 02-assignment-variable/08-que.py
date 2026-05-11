b044aa2ecf1f903eec18f2243e87bd47726a904c
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-11 18:11:19 +0530

    08-que.py

---
+++ 02-assignment-variable/08-que.py
@@ -0,0 +1,7 @@
+email = input("enter your email address:")
+
+if "gmail.com" in email:
+    print("elegible for registration. gmail domain expected.")
+
+else:
+    print("not eligible for registration. only Gmail address are allowed")    
