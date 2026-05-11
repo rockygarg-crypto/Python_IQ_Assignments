3fb0c5d7d654a1832069c8c1b49627316b2ae034
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-11 16:34:43 +0530

    new file

---
+++ 02-assignment-variable/06-que.py
@@ -0,0 +1,16 @@
+age = 30
+monthly_income = 30000
+credit_score = 750
+outstanding_debt = 15000
+
+if (18 <= age <= 60 and  
+    monthly_income >= 25000 and
+    credit_score >= 700 and
+    outstanding_debt <= 15000) :
+   
+    print("loan approved")
+else:   
+    print("loan rejected")
+
+
+
